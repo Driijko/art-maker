2f6bf8c8dc379cc26692318fd65761d69cae7357
Driijko <driijko@gmail.com>
2024-07-23 13:07:03 -0400

    First commit.

---
+++ my-app/src/App.svelte
@@ -0,0 +1,21 @@
+<!-- SCRIPTS ////////////////////////////////////////////// -->
+<script>
+  // IMPORTS ------------------------------------------
+  import CircleBush from "./components/CircleBush.svelte";
+
+</script>
+
+<!-- MARKUP ////////////////////////////////////////////// -->
+<div>{test}</div>
+<!-- <div class="canvas">
+  <CircleBush />
+</div> -->
+
+<!-- STYLES ////////////////////////////////////////////// -->
+<style>
+.canvas {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ my-app/src/components/CircleBush.svelte
@@ -0,0 +1,29 @@
+<!-- SCRIPTS ////////////////////////////////////// -->
+<script>
+  // SVG VARIABLES ------------------------------------------
+  const svgSize = {width: "500px", height: "1000px"};
+  const svgPosition = {x: 100, y: 200};
+
+  // CIRCLE VARIABLES ---------------------------------------
+  const circleSizeRange = {low: 10, high: 20};
+
+  // ARRAY -----------------------------
+  // const array = new Array(number);
+
+</script>
+
+<!-- MARKUP ///////////////////////////////////////// -->
+<svg style:width={svgSize.width}
+  style:height={svgSize.height}
+  style:left={svgPosition.x}
+  style:top={svgPosition.y}
+>
+</svg>
+
+<!-- STYLES ///////////////////////////////////////////// -->
+<style>
+svg {
+  position: absolute;
+  border: 4px solid green;
+}
+</style>
