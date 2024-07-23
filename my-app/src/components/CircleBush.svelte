242af588b8302dbacb2e780cad975582cf711d58
Driijko <driijko@gmail.com>
2024-07-23 17:00:24 -0400

    CircleBush: clip-paths.

--- my-app/src/components/CircleBush.svelte
+++ my-app/src/components/CircleBush.svelte
@@ -4,22 +4,30 @@
   import randomRange from "../scripts/randomRange";
 
   // SVG VARIABLES ------------------------------------------
-  const svgSize = {width: 500, height: 1000};
-  const svgPosition = {x: 100, y: 200};
+  const svgSize = {width: window.innerWidth, height: window.innerHeight};
+  const svgPosition = {x: 0, y: 0};
 
   // CIRCLE VARIABLES ---------------------------------------
-  const number = 300;
-  const size = {min: 10, max: 20};
-  const strokeSize = {min: 1, max: 2};
-  const opacity = 0.5;
+  const number = 100;
+  const size = {min: 10, max: 100};
+  const strokeSize = {min: 1, max: 5};
+  const opacity = 1;
   const color = {
-    hue: {min:0, max:5}, 
+    hue: {min:100, max:105}, 
     saturation: {min: 90, max: 100}, 
-    lightness: {min: 50, max: 55}
+    lightness: {min: 80, max: 85}
+  };
+  const strokeColor = {
+    hue: {min: 100, max: 105},
+    saturation: {min: 90, max: 100},
+    lightness: {min: 80, max: 90},
   };
 
   // ARRAY -----------------------------
-  const array = new Array(number);
+  const array = [];
+  for (let i = 0 ; i < number ; i++) {
+    array[i] = {x: randomRange(0, window.innerWidth), y: randomRange(0, window.innerHeight)};
+  };
 
 </script>
 
@@ -29,14 +37,37 @@
   style:left={svgPosition.x}
   style:top={svgPosition.y}
 >
+  <!-- <g id="circles">
   {#each array as circle}
-    <circle cx={randomRange(0,svgSize.width)} cy={randomRange(0,svgSize.height)} 
+    <circle cx={circle.x} cy={circle.y} 
       r={randomRange(size.min, size.max)}
       fill={`hsla(${randomRange(color.hue.min, color.hue.max)}, ${randomRange(color.saturation.min, color.saturation.max)}%, ${randomRange(color.lightness.min, color.lightness.max)}%, ${opacity})`}
-      stroke={`hsla(0, 100%, 100%, ${opacity})`}
+      stroke={`hsla(
+      ${randomRange(strokeColor.hue.min, strokeColor.hue.max)}, 
+      ${randomRange(strokeColor.saturation.min, strokeColor.saturation.max)}%, ${randomRange(strokeColor.lightness.min, strokeColor.lightness.max)}%, ${opacity})`}
       stroke-width={randomRange(strokeSize.min,strokeSize.max)}
     />
   {/each}
+  </g> -->
+  <!-- <clipPath id="cp">
+    <use href="#circles" />
+  </clipPath> -->
+  <!-- <image clip-path="url(#cp)" href="./image1.jpg" height={window.innerHeight} x="0" y="0" /> -->
+   <defs>
+    <clipPath id="circles">
+      {#each array as circle}
+      <circle cx={circle.x} cy={circle.y} 
+        r={randomRange(size.min, size.max)}
+        fill={`hsla(${randomRange(color.hue.min, color.hue.max)}, ${randomRange(color.saturation.min, color.saturation.max)}%, ${randomRange(color.lightness.min, color.lightness.max)}%, ${opacity})`}
+        stroke={`hsla(
+        ${randomRange(strokeColor.hue.min, strokeColor.hue.max)}, 
+        ${randomRange(strokeColor.saturation.min, strokeColor.saturation.max)}%, ${randomRange(strokeColor.lightness.min, strokeColor.lightness.max)}%, ${opacity})`}
+        stroke-width={randomRange(strokeSize.min,strokeSize.max)}
+      />
+    {/each}
+    </clipPath>
+   </defs>
+   <image clip-path="url(#circles)" href="./image1.jpg" width={window.innerWidth} x="0" y="0" />
 </svg>
 
 <!-- STYLES ///////////////////////////////////////////// -->
@@ -44,5 +75,8 @@
 svg {
   position: absolute;
   border: 4px solid hsla(0, 100%, 10%);
+}
+image {
+  opacity: 1;
 }
 </style>
